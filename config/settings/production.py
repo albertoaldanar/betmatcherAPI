c0590025efd79f4e489f9c9433b17554ea6ba23f
Alberto Aldana <albertoaldanar@hotmail.com>
2021-03-03 14:12:29 -0700

    changes in backend

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,7 +5,7 @@ from .base import env
 
 # Base
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['betmatcherapi.com'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['http://104.251.210.126:8000'])
 
 # Databases
 DATABASES['default'] = env.db('DATABASE_URL')  # NOQA
